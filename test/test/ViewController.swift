281c67fec9ba9f5ae01fcb33f07aa6cb14bbd6ca
David <dwagg@pdx.edu>
2017-05-27 04:37:02 -0700

    Added a user and password text field and made the button call jama api to get user

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonOutlet: UIButton!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTouchUp(_ sender: Any) {
+        print("Button touched, calling to get current user")
+        restCall()
+    }
+    
+    func restCall() {
+        let user = username.text
+        let pass = password.text
+        let loginString = user! + ":" + pass!
+        let loginData = loginString.data(using: String.Encoding.utf8)
+        let base64LoginString = loginData?.base64EncodedString()
+        
+        let endpoint: String = "https://capstone-sandbox.jamacloud.com/rest/latest/users/current"
+        guard let url = URL(string: endpoint) else {
+            print("Bad string for url")
+            return
+        }
+        
+        //let cred = URLCredential(user: "dwagg", password: "Bewithyou23", persistence: .none)
+    
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("Basic \(base64LoginString ?? "")", forHTTPHeaderField: "Authorization")
+        let session = URLSession.shared
+  
+        let task = session.dataTask(with: urlRequest) {
+            (data,response,error) in
+            guard error == nil else {
+                print("error calling endpoint")
+                print(error as Any)
+                return
+            }
+            guard let responseData = data else {
+                print("Error did not recieve data")
+                return
+            }
+            do {
+                guard let jsonData = try JSONSerialization.jsonObject(with: responseData, options: [])
+                    as? [String: Any] else {
+                        print("error trying to convert to JSON")
+                        return
+                }
+                print("The jsonData is\n" + jsonData.description)
+                print("Count = " + jsonData.count.description)
+
+            } catch {
+                print("error trying to convert to json")
+            }
+        }
+        
+        task.resume()
+    }
 
 }
 
